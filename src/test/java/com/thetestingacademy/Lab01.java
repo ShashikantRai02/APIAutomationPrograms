d86f804335bad7490cfd8b0a2010ef16280410cf
ShashikantRai02 <140936627+ShashikantRai02@users.noreply.github.com>
2026-03-23 14:40:02 +0530

    23rdmarch2026

---
+++ src/test/java/com/thetestingacademy/Lab01.java
@@ -0,0 +1,14 @@
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        RestAssured.given()
+                .baseUri("https://reqres.in/api/users")
+                .when()
+                .get()
+                .then()
+                .log().all();
+    }
+}
